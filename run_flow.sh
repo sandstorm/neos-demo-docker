a79e70a4445c0400598e75bf43bbd3bdb30674de
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2018-11-20 13:38:24 +0100

    TASK: add default user

--- run_flow.sh
+++ run_flow.sh
@@ -6,5 +6,9 @@ sleep 5
 ./flow doctrine:migrate
 ./flow site:import Neos.Demo
 ./flow resource:publish
+
+./flow user:create admin password Admin Admax
+./flow user:addrole admin Neos.Neos:Administrator
+
 ./flow cache:warmup
-./flow server:run
+./flow server:run --host=0.0.0.0
